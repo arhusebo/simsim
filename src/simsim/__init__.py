3e3694b424e4a7f4819f328006b1b8d2896e6b0e
Arild Bergesen Husebø <arhusebo@gmail.com>
2024-06-24 15:23:50 +0200

    remove unused code

--- src/simsim/__init__.py
+++ src/simsim/__init__.py
@@ -1,26 +1,11 @@
 import os
-import sys
 import pickle
 import functools
 import logging
-import importlib
 
-
-autoreload = False
 
 logger = logging.getLogger("__main__")
 logging.basicConfig(level=logging.INFO)
-
-
-def _do_autoreload(func):
-    global autoreload
-    if autoreload:
-        mod = sys.modules[func.__module__]
-        importlib.reload(mod)
-        logger.debug(f"Module {func.__module__} was reloaded")
-        newfunc = getattr(mod, func.__name__)
-        return newfunc
-    return func
 
 
 def _get_path(path: str, name: str):
@@ -32,7 +17,6 @@ def experiment(path: str):
     def decorator_experiment(func):
         @functools.wraps(func)
         def wrapper():
-            #func1 = _do_autoreload(func)
             name = func.__name__
             logger.info(f"Running experiment '{name}'")
             results = func()
@@ -53,7 +37,6 @@ def presentation(path: str, name: str | list[str]):
     def decorator_presentation(presentation_func):
         @functools.wraps(presentation_func)
         def wrapper():
-            # load results
             results = list()
             if isinstance(name, str):
                 name_list = [name]
