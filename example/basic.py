3f33a80dc1c0d219499a8dd2bd712308c038f1bd
Arild Bergesen Husebø <arhusebo@gmail.com>
2024-09-19 11:37:11 +0200

    add experiment registry

--- example/basic.py
+++ example/basic.py
@@ -3,7 +3,7 @@ import simsim
 
 
 @simsim.experiment(path="./experiments/")
-def fibonacci():
+def fibonacci() -> list[float]:
     x = [0, 1]
     for i in range(20):
         x.append(x[i]+x[i+1])
@@ -19,12 +19,12 @@ def primes():
     return prm
 
 
-@simsim.presentation(path="./experiments/", name="fibonacci")
+@simsim.presentation(fibonacci)
 def present_fibonacci(x):
     print("Fibonacci sequence: "+", ".join(str(val) for val in x))
 
 
-@simsim.presentation(path="./experiments/", name=["fibonacci", "primes"])
+@simsim.presentation(fibonacci, primes)
 def present_multiple(results):
     print("Fibonacci sequence: "+", ".join(str(val) for val in results[0]))
     print("Prime sequence: "+", ".join(str(val) for val in results[1]))
--- src/simsim/__init__.py
+++ src/simsim/__init__.py
@@ -3,32 +3,33 @@ import pickle
 import functools
 import logging
 import time
+import collections
 
 
 logger = logging.getLogger("__main__")
 logging.basicConfig(level=logging.INFO)
 
 
-def _get_path(path: str, name: str):
-    path_ = pathlib.Path(path)
-    fname = f"{name}.pkl"
-    return path_.joinpath(fname)
+_registry = {}
 
 
 def experiment(path: str):
     def decorator_experiment(func):
+
+        path_ = pathlib.Path(path)
+        name = func.__name__
+        fp = (path_/name).with_suffix(".pkl")
+        _registry[name] = fp 
+
         @functools.wraps(func)
         def wrapper():
-            name = func.__name__
             logger.info(f"Running experiment '{name}'")
             time0 = time.time_ns()
             results = func()
             time_total = time.time_ns() - time0
 
-            path_ = pathlib.Path(path)
             path_.mkdir(exist_ok=True)
-
-            with open(_get_path(path, name), "wb") as f:
+            with open(fp, "wb") as f:
                 pickle.dump(results, f)
 
             logger.info(f"Experiment '{name}' terminated successfully in {time_total/1.e9} s")
@@ -37,18 +38,15 @@ def experiment(path: str):
     return decorator_experiment
 
 
-def presentation(path: str, name: str | list[str]):
+def presentation(*experiments):
     def decorator_presentation(presentation_func):
         @functools.wraps(presentation_func)
         def wrapper():
-            results = list()
-            if isinstance(name, str):
-                name_list = [name]
-            else:
-                name_list = name
-            for nm in name_list:
-                with open(_get_path(path, nm), "rb") as f:
-                    results.append(pickle.load(f))
+            results = []
+            for ex in experiments:
+                with open(_registry[ex.__name__], "rb") as f:
+                    res = pickle.load(f)
+                    results.append(res)
             presentation_func(results if len(results) > 1 else results[0])
         
         return wrapper
